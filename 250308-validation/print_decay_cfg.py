ec16abbf3f99bb83852c8d54f51fc64a8fbe4043
Jongwon Shin <eigen1907@gmail.com>
2025-03-19 16:55:07 +0900

    update 250319

---
+++ 250308-validation/print_decay_cfg.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_2025_cff import Run3_2025
+
+process = cms.Process('PRINT',Run3_2025)
+
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.printTree = cms.EDAnalyzer("ParticleListDrawer",
+  maxEventsToPrint = cms.untracked.int32(-1),
+  printVertex = cms.untracked.bool(False),
+  printOnlyHardInteraction = cms.untracked.bool(False), # Print only status=3 particles. This will not work for Pythia8, which does not have any such particles.
+  src = cms.InputTag("genParticles")
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+process.source = cms.Source("PoolSource",
+    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+    fileNames = cms.untracked.vstring('file:step1.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.p = cms.Path(process.printTree)
